8d4edad71298c57e5c516c7db5ca42e3d45fa9a9
ChannyKHIN <khinchanny@hotmail.com>
2018-09-15 17:20:04 +0700

    add project

---
+++ mysite/polls/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    # ...
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
